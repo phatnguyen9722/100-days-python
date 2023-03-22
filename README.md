5b50d48cb09e179c4ec0422f067d5fa8d8b36d73
ntanphat2 <ntanphat2@tma.com>
2023-03-22 08:51:08 +0700

    day 6

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Day 1: Use print to show output</br>
 Day 2: Use input method</br>
 Day 3: Concat variable and sentences</br>
 Day 4: Complex sentence</br>
-Day 5: Python conditions and if statements<br>
+Day 5: Python conditions and if statements</br>
+Day 6: Make a basic username and password => Log in</br>
---
+++ day_6.py
@@ -0,0 +1,35 @@
+# Fix Code 1
+# print("SECURE LOGIN")
+# username = input("Username > ")
+
+# if username == "mark":
+#   print("Welcome Mark!")
+# elif username == "suzanne":
+#   print("Hey there Suzanne!")
+# else:
+#   print("Go away!")
+
+# Fix Code 2
+# season = input("what is your favorite season?")
+# if season == "spring":
+#   print("Ah! The birds are chirping and flowers blooming.")
+# elif season == "summer":
+#   print("Catch some sun and cool off with a lemonade.")
+# elif season == "autumn":
+#   print("The leaves are changing and the air is crisp. Enjoy!")
+# elif season == "winter":
+#   print("Stay warm by the fire and watch the snow fall.")
+# else: 
+#   print("I don't know that season. Please try again.")
+
+userName = input("User Name: ")
+passWord = input("Your Password: ")
+
+if userName == "ntanphat" and passWord == "123":
+  print("Hello Guy, have a beautifulday")
+elif userName == "ntanphat2" and passWord == "1234":
+  print("You will have a successful year")
+elif userName == "ntanphat3" and passWord == "12345":
+  print("Wish you have a happy year <3")
+else:
+  print("Wrong Username or Password!")
